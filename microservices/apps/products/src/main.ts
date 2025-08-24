64379a3290ea1f020a9cbb43f81cb9ddbeffe55f
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2025-08-25 05:59:35 +0600

    Config All Services for Microservices main.ts

--- microservices/apps/products/src/main.ts
+++ microservices/apps/products/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: process.env.MICROSERVICE_HOST ?? 'localhost',
+      port: process.env.MICROSERVICE_PORT ? parseInt(process.env.MICROSERVICE_PORT, 10) : 4002,
+    },
+  });
+  await app.listen();
+  console.log(`Microservice is listening on ${process.env.MICROSERVICE_HOST ?? 'localhost'}:${process.env.MICROSERVICE_PORT ? parseInt(process.env.MICROSERVICE_PORT, 10) : 4002}`);
 }
 bootstrap();
